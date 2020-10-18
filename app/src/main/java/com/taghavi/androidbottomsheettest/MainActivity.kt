6954c3be5bdef95712e959e1144c0e215d4b1e7f
Ehsan Taghavi <ehsantgv@gmail.com>
2020-10-18 16:51:49 +0330

    MainActivity implement basic designs

--- app/src/main/java/com/taghavi/androidbottomsheettest/MainActivity.kt
+++ app/src/main/java/com/taghavi/androidbottomsheettest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.taghavi.androidbottomsheettest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainTextView.setOnClickListener {
+            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
